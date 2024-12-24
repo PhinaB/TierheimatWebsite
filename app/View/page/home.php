f2f52248f1521cc827f9336192473597f5b09f04
Stephanie <103069877+gymStephanie@users.noreply.github.com>
2024-12-24 21:42:43 +0100

    other structur home page

--- app/View/page/home.php
+++ app/View/page/home.php
@@ -7,39 +7,20 @@
 
 <div id="page" class="hidden">
     <div class="tile">
-        <h2>Aktuelles</h2>
+        <h2 class="marginTopH2">Unsere Tiere</h2>
         <hr class="underHeadline" />
 
-        <div class="box-container box-container-button">
-            <div>
-                <img src="../public/img/ausbauTierheim.jpg" alt="Lageplan des Tierheims Tierheimat" title="Lageplan des Tierheims Tierheimat" draggable="false" />
-                <h3>Erweiterung des Tierheims</h3>
-                <p>
-                    Dank Ihrer großzügigen Spenden und Unterstützung konnten wir neue Unterkünfte und moderne Einrichtungen für unsere Tiere schaffen.
-                </p><p>
-                    Diese Erweiterung ermöglicht es uns ...
-                </p>
-                <a href="aktuellesWeiterlesenAusbauTierheim.php" class="button" draggable="false"><i class="fa-solid fa-newspaper"></i> Weiterlesen</a>
+        <div class="box-container" id="copyAllAnimalsHere"></div>
+
+        <div class="hidden" id="hiddenAnimal">
+            <div class="aussenboxBildwechselKlein">
+                <a class="bildwechsel" draggable="false">&nbsp;</a>
             </div>
-            <div>
-                <img src="../public/img/tierheimFest.jpg" alt="Werbebild für das Tierheimfest" title="Werbebild für das Tierheimfest" draggable="false" />
-                <h3>15 Jahre Tierheimat</h3>
-                <p>
-                    Unser Tierheim feiert dieses Jahr sein 15-jähriges Bestehen und blickt auf eine bewegte Geschichte zurück. Seit unserer Gründung  ...
-                </p>
-                <a href="aktuellesWeiterlesen.php" class="button" draggable="false"><i class="fa-solid fa-newspaper"></i> Weiterlesen</a>
-            </div>
-            <div>
-                <img src="../public/img/glücklicheKatze.jpg" alt="Bild einer glücklichen Katze" title="Bild einer glücklichen Katze" draggable="false" />
-                <h3>Erfolgreiche Tiervermittlung</h3>
-                <p>
-                    In den letzten Monaten konnten wir zahlreiche Tiere erfolgreich in liebevolle Zuhause vermitteln.
-                </p><p>
-                    Wir sind stolz auf die Fortschritte, die ...
-                </p>
-                <a href="current.php#tiervermittlung" class="button" draggable="false"><i class="fa-solid fa-newspaper"></i> Weiterlesen</a>
-            </div>
+            <h3></h3>
+            <p class="beschreibungBeginn"></p>
         </div>
+
+        <a href="unsereTiere" class="button marginTopButton" draggable="false"><i class="fa-solid fa-plus"></i> Weitere Tiere ansehen</a>
     </div>
     <div class="tile">
         <h2 class="marginTopH2">Unser Team</h2>
@@ -86,19 +67,37 @@
         </div>
     </div>
     <div class="tile">
-        <h2 class="marginTopH2">Unsere Tiere</h2>
+        <h2>Aktuelles</h2>
         <hr class="underHeadline" />
 
-        <div class="box-container" id="copyAllAnimalsHere"></div>
-
-        <div class="hidden" id="hiddenAnimal">
-            <div class="aussenboxBildwechselKlein">
-                <a class="bildwechsel" draggable="false">&nbsp;</a>
+        <div class="box-container box-container-button">
+            <div>
+                <img src="../public/img/ausbauTierheim.jpg" alt="Lageplan des Tierheims Tierheimat" title="Lageplan des Tierheims Tierheimat" draggable="false" />
+                <h3>Erweiterung des Tierheims</h3>
+                <p>
+                    Dank Ihrer großzügigen Spenden und Unterstützung konnten wir neue Unterkünfte und moderne Einrichtungen für unsere Tiere schaffen.
+                </p><p>
+                    Diese Erweiterung ermöglicht es uns ...
+                </p>
             </div>
-            <h3></h3>
-            <p class="beschreibungBeginn"></p>
+            <div>
+                <img src="../public/img/tierheimFest.jpg" alt="Werbebild für das Tierheimfest" title="Werbebild für das Tierheimfest" draggable="false" />
+                <h3>15 Jahre Tierheimat</h3>
+                <p>
+                    Unser Tierheim feiert dieses Jahr sein 15-jähriges Bestehen und blickt auf eine bewegte Geschichte zurück. Seit unserer Gründung  ...
+                </p>
+            </div>
+            <div>
+                <img src="../public/img/glücklicheKatze.jpg" alt="Bild einer glücklichen Katze" title="Bild einer glücklichen Katze" draggable="false" />
+                <h3>Erfolgreiche Tiervermittlung</h3>
+                <p>
+                    In den letzten Monaten konnten wir zahlreiche Tiere erfolgreich in liebevolle Zuhause vermitteln.
+                </p><p>
+                    Wir sind stolz auf die Fortschritte, die ...
+                </p>
+            </div>
         </div>
-        
-        <a href="unsereTiere" class="button marginTopButton" draggable="false"><i class="fa-solid fa-plus"></i> Weitere Tiere ansehen</a>
+
+        <a href="aktuelles" class="button marginTopButton" draggable="false"><i class="fa-solid fa-newspaper"></i> Alle Beiträge ansehen</a>
     </div>
 </div>
