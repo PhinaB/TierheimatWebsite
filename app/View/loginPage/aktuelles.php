7848b29062ce112460d9791930e1e54ee0fb2b71
Lucas-Manfred <herpelucas@gmail.com>
2025-01-02 19:59:31 +0100

    changes article and include dynamical feature

--- app/View/loginPage/aktuelles.php
+++ app/View/loginPage/aktuelles.php
@@ -9,7 +9,6 @@
     <link rel="stylesheet" href="../../../public/css/footer.css" />
     <link rel="stylesheet" href="../../../public/css/loggedInPage.css" />
     <link rel="stylesheet" href="../../../public/css/aktuelles.css" />
-
     <link rel="stylesheet" href="../../../public/lib/fontawesome-6.5.2/css/all.min.css">
 
     <title>Tierheimat</title>
@@ -28,68 +27,37 @@
 <body>
 <header>
     <nav>
-            <span class="linkLogo">
-                <a href="indexLogin.html" class="logo" draggable="false">
-                    <img class="logoPicture" src="../../../public/img/logo.jpg" alt="Logo" title="Logo - mit Linksklick geht's zur Startseite" draggable="false" />
-                </a>
-            </span>
-
+        <span class="linkLogo">
+            <a href="indexLogin.html" class="logo" draggable="false">
+                <img class="logoPicture" src="../../../public/img/logo.jpg" alt="Logo" title="Logo - mit Linksklick geht's zur Startseite" draggable="false" />
+            </a>
+        </span>
         <span class="user">
-                <i class="fa-solid fa-user"></i> Nutzername
-            </span>
+            <i class="fa-solid fa-user"></i> Nutzername
+        </span>
 
         <div id="menuRight">
             <a href="indexLogin.html" draggable="false">
                 <i class="fa-solid fa-house menuIcon onlySmallMenu"></i>
                 Start
             </a>
-            <div class="dropdown">
-                <a class="deactivate" draggable="false">
-                    <i class="fa-solid fa-paw menuIcon onlySmallMenu"></i>
-                    Unsere <br class="onlySmallMenu" />Tiere
-                </a>
-                <div class="submenu smallWidthSubmenu">
-                    <a href="unsereTiere.html">Alle Tiere</a>
-                    <a href="unsereHunde.html">Hunde</a>
-                    <a href="unsereKatzen.html">Katzen</a>
-                    <a href="unsereKleintiere.html">Kleintiere</a>
-                    <a href="unsereExoten.html">Exoten</a>
-                </div>
-            </div>
             <a class="disabled" draggable="false">
                 <i class="fa-solid fa-calendar menuIcon onlySmallMenu"></i>
                 Aktuelles
-            </a>
-            <div class="dropdown">
-                <a class="deactivate" draggable="false">
-                    <i class="fa-solid fa-magnifying-glass-location menuIcon onlySmallMenu"></i>
-                    Vermisst/<br class="onlySmallMenu" />Gefunden
-                </a>
-                <div class="submenu bigWidthSubmenu">
-                    <a href="vermisstGefunden.html">Alle Tiere</a>
-                    <a href="vermisst.html">Vermisste Tiere</a>
-                    <a href="gefunden.html">Gefundene Tiere</a>
-                </div>
-            </div>
-            <a href="serviceInfos.html" draggable="false">
-                <i class="fa-solid fa-circle-info menuIcon onlySmallMenu"></i>
-                Service/<br class="onlySmallMenu" />Infos
-            </a>
-            <a href="../index.html" draggable="false">
-                <i class="fa-solid fa-right-from-bracket menuIcon onlySmallMenu"></i>
-                Logout
             </a>
         </div>
     </nav>
 </header>
 <div class="grid">
     <main>
+        <!-- Breadcrumb Navigation -->
         <div class="tile tileBorder">
             <a href="indexLogin.html" draggable="false"><i class="fa-solid fa-house"></i> Startseite</a>
             >
             <a class="disabled" draggable="false">Aktuelles</a>
         </div>
 
+        <!-- Überschrift -->
         <div class="tile">
             <h2>Neues aus dem Tierheim</h2>
             <hr class="underHeadline" />
@@ -98,43 +66,34 @@
                 <div class="text">
                     <p>
                         Sehr geehrte Unterstützer und Freunde unseres Tierheims,
-                    </p><br /><p>
+                    </p><br />
+                    <p>
                         wir freuen uns sehr, Ihnen mitteilen zu können, dass die bauliche Erweiterung unseres Tierheims erfolgreich begonnen hat.
                     </p>
-
-                    <div class="hiddenOnSmallScreen">
-                        <p>
-                            Dank Ihrer großzügigen Spenden und Unterstützung konnten wir neue Unterkünfte und moderne Einrichtungen für unsere Tiere schaffen.
-                        </p><br /><p>
-                            Diese Erweiterung ermöglicht es uns noch mehr Tieren in Not zu helfen und ihnen ein vorübergehendes Zuhause zu bieten.
-                            Ein herzliches Dankeschön an alle Spender, freiwilligen Helfer und Partner, die dieses Projekt möglich gemacht haben.
-                        </p>
-                    </div>
-                    <div class="hiddenOnBigScreen">
-                        <p>Dank Ihrer großzügigen Spenden und ... </p>
-                        <br />
-                        <a href="aktuellesWeiterlesenAusbauTierheim.html" draggable="false" class="button">
-                            <i class="fa-solid fa-newspaper"></i> Zum Artikel
-                        </a>
-                    </div>
+                    <br />
+                    <a href="aktuellesWeiterlesenAusbauTierheim.html" draggable="false" class="button">
+                        <i class="fa-solid fa-newspaper"></i> Zum Artikel
+                    </a>
                 </div>
             </div>
         </div>
 
+        <!-- Dynamische Artikel -->
         <div class="tile">
             <h2>Weitere Artikel</h2>
             <hr class="underHeadline" />
+
+            <!-- Container für dynamisch geladene Artikel -->
             <div id="articlesContainer" class="box-container"></div>
 
-            <a id="weitereArtikelAnzeigen" class="button hidden">
-                Weitere Artikel anzeigen
-            </a>
-
+            <!-- Ladeanimation -->
             <div id="loading" class="hidden">Laden...</div>
             <input type="hidden" name="offset" value="0" />
             <div class="fehlerLoading"></div>
         </div>
     </main>
+
+    <!-- Footer -->
     <footer>
         <div>
             <h4>KONTAKT</h4>
--- public/js/aktuelles.js
+++ public/js/aktuelles.js
@@ -1,76 +1,40 @@
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener("DOMContentLoaded", function () {
+    const articlesContainer = document.getElementById("articlesContainer");
+    const loading = document.getElementById("loading");
+
+    // Lade Artikel dynamisch
+    function loadArticles() {
+        loading.classList.remove("hidden");
+
+        fetch("getArticles.php") // PHP-Datei aufrufen
+            .then(response => response.json())
+            .then(articles => {
+                loading.classList.add("hidden");
+
+                articles.forEach(article => {
+                    const articleDiv = document.createElement("div");
+                    articleDiv.classList.add("flexWir", "tileBorder");
+
+                    articleDiv.innerHTML = `
+                        <img src="${article.image}" alt="${article.title}" draggable="false">
+                        <div class="text">
+                            <h3>${article.title}</h3>
+                            <p>${article.excerpt}</p>
+                            <a href="${article.link}" draggable="false" class="button">
+                                <i class="fa-solid fa-newspaper"></i> Zum Artikel
+                            </a>
+                        </div>
+                    `;
+
+                    articlesContainer.appendChild(articleDiv);
+                });
+            })
+            .catch(error => {
+                console.error("Fehler beim Laden der Artikel:", error);
+                loading.innerText = "Fehler beim Laden der Artikel.";
+            });
+    }
+
+    // Initial Artikel laden
     loadArticles();
-    document.querySelector('input[name=offset]').value = '0';
-
-    document.querySelector('#weitereArtikelAnzeigen').addEventListener('click', function () {
-        let offset = document.querySelector('input[name=offset]');
-        offset.value = parseInt(offset.value) + 6; // Lade 6 weitere Artikel
-        loadArticles();
-    });
 });
-
-function loadArticles() {
-    let spinner = document.getElementById('loading');
-    spinner.classList.remove('hidden');
-
-    let offset = document.querySelector('input[name=offset]').value;
-
-    const xhttp = new XMLHttpRequest();
-
-    xhttp.onreadystatechange = function () {
-        if (xhttp.readyState === 4) {
-            if (xhttp.status === 200) {
-                let response = JSON.parse(this.response);
-
-                if (response.artikel.length > 0) {
-                    setArticlesToPage(response.artikel);
-                } else {
-                    document.querySelector('.fehlerLoading').innerText = "Keine weiteren Artikel vorhanden!";
-                }
-
-                if (response.countedArticles > parseInt(offset) + 6) {
-                    document.querySelector('#weitereArtikelAnzeigen').classList.remove('hidden');
-                }
-            } else {
-                document.querySelector('.fehlerLoading').innerText = "Fehler beim Laden der Artikel!";
-            }
-
-            spinner.classList.add('hidden');
-        }
-    };
-
-    xhttp.open('POST', '../public/load/all/articles');
-    xhttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-    xhttp.send('offset=' + offset);
-}
-
-function setArticlesToPage(articles) {
-    let container = document.querySelector('#articlesContainer');
-
-    articles.forEach(article => {
-        let articleDiv = document.createElement('div');
-        articleDiv.classList.add('article-box');
-
-        let img = document.createElement('img');
-        img.src = "../public/img/" + article.Bildadresse;
-        img.alt = article.Ueberschrift;
-
-        let title = document.createElement('h3');
-        title.innerText = article.Ueberschrift;
-
-        let description = document.createElement('p');
-        description.innerText = article.Text.substring(0, 100) + '...';
-
-        let button = document.createElement('a');
-        button.href = 'aktuellesWeiterlesen.html?id=' + article.ArtikelID;
-        button.classList.add('button');
-        button.innerHTML = '<i class="fa-solid fa-newspaper"></i> Zum Artikel';
-
-        articleDiv.appendChild(img);
-        articleDiv.appendChild(title);
-        articleDiv.appendChild(description);
-        articleDiv.appendChild(button);
-
-        container.appendChild(articleDiv);
-    });
-}
