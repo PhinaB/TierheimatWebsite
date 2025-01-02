cd55f22d0f9c5657772b6ad9c7c14f15a6ee4f1f
PhinaB <josephina.burger@fh-erfurt.de>
2025-01-02 23:51:38 +0100

    remove config from index.php

--- public/index.php
+++ public/index.php
@@ -1,7 +1,6 @@
 <?php
 use core\Route;
 
-require_once __DIR__ . '/../config/config.php';
 require_once __DIR__ . '/../core/Route.php';
 require_once __DIR__ . '/../app/Controller/StaticPageController.php';
 require_once __DIR__ . '/../app/Controller/missingFoundAnimalController.php';
