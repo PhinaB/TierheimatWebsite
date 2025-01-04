465cad530e68e356835ead585a28e2e7467e4c53
Lucas-Manfred <herpelucas@gmail.com>
2025-01-04 16:06:19 +0100

    MVC Update

--- app/View/documentation/documentationDWP1.php
+++ app/View/documentation/documentationDWP1.php
@@ -19,12 +19,12 @@
     <!--
     TODO WICHTIG!!!!
 
-    -ERM und relationales Modell für die DB!
+    -ERM und relationales Modell für die DB!  → integriert
     -Rollenmodell für die Nutzer
     -Flussbild für Dateneingabe per mehrseitigem Formular mit Validierung
     -Beschreibung der Funktionalitäten für jede Seite (beschriftete Screenshots)
-     -> Hinweis v.a. auf besondere techn. Anforderungen, bekannte Fehler, unvollständige Implementierung, "versteckte" Features, ...
-    -Auflistung Tätigkeiten jeder Person und benötigter Aufwand
+     -> Hinweis v.a. auf besondere techn. Anforderungen, bekannte Fehler, unvollständige Implementierung, "versteckte" Features, ... → abgeschlossen, ist z.B mit der favourite Funktion benannnt, dass diese als Cookies gespeichert werden und nicht in der DB
+    -Auflistung Tätigkeiten jeder Person und benötigter Aufwand → integriert bis auf die h per person
 
     -->
 
