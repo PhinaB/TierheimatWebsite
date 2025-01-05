3f60585f41e7a684778114f05ae0a88b05c72ac5
PhinaB <josephina.burger@fh-erfurt.de>
2025-01-05 18:20:09 +0100

    select in MissingFoundAnimals

--- public/js/dynamicMissingFoundAnimals.js
+++ public/js/dynamicMissingFoundAnimals.js
@@ -1,14 +1,64 @@
-document.addEventListener("DOMContentLoaded", function(){
+document.addEventListener("DOMContentLoaded", function() {
     checkLoginStatus();
-    loadMissingFoundAnimalsToPage();
-
-})
-
-function loadMissingFoundAnimalsToPage(){
 
     const type = document.getElementById('currentMissingOrFound').value;
+    loadMissingFoundAnimalsToPage(type);
 
-    const missingAnimalContainer = document.getElementById('missingAnimals');
+    document.querySelector('#selectAnimalStatus').addEventListener('submit', function (event) {
+        event.preventDefault();
+        let formSubmitted = true;
+
+        const selectType = document.querySelector('#tierstatusAuswählen').value;
+
+        loadMissingFoundAnimalsToPage(selectType);
+
+
+        if (!formSubmitted) {
+            const type = document.getElementById('currentMissingOrFound').value;
+            loadMissingFoundAnimalsToPage(type);
+        }
+
+    })
+})
+
+function setBack() {
+
+    const copyAllFoundAnimalsHere = document.querySelector('#copyAllFoundAnimalsHere');
+    if (copyAllFoundAnimalsHere) {
+        copyAllFoundAnimalsHere.innerHTML = "";
+    }
+    const copyAllMissingAnimalsHere = document.querySelector('#copyAllMissingAnimalsHere');
+    if (copyAllMissingAnimalsHere) {
+        copyAllMissingAnimalsHere.innerHTML = "";
+    }
+
+    const headingMissingAnimals = document.querySelector('#headingMissingAnimals');
+    if (headingMissingAnimals){
+        headingMissingAnimals.innerHTML= "";
+        document.querySelector('#missingUnderHeadline').classList.add('hidden');
+    }
+
+    const headingFoundAnimals = document.querySelector('#headingFoundAnimals');
+    if (headingFoundAnimals){
+        headingFoundAnimals.innerHTML= "";
+        document.querySelector('#foundUnderHeadline').classList.add('hidden');
+    }
+
+    const copyFirstFoundAnimalHere = document.querySelector('#copyFirstFoundAnimalHere');
+    if(copyFirstFoundAnimalHere){
+        copyFirstFoundAnimalHere.innerHTML="";
+    }
+    const copyFirstMissingAnimalHere = document.querySelector('#copyFirstMissingAnimalHere');
+    if(copyFirstMissingAnimalHere){
+        copyFirstMissingAnimalHere.innerHTML="";
+    }
+
+}
+
+    function loadMissingFoundAnimalsToPage(type){
+
+    setBack();
+
 
     let formData= new FormData();
     formData.append('type', type)
@@ -23,9 +73,11 @@ function loadMissingFoundAnimalsToPage(){
         .then(data => {
             console.log('Serverantwort:', data);
             
-            if(type === "Vermisst / Gefundene Tiere"){
+            if(type === "Vermisste / Gefundene Tiere") {
                 displayAnimals(data.missingAnimals, "Vermisste Tiere");
                 displayAnimals(data.foundAnimals, "Gefundene Tiere");
+                console.log('Missing ANimals:', data.missingAnimals); // Gibt das missingAnimals-Array aus
+                console.log('Found Anials:', data.foundAnimals);
             }
             else {
                 displayAnimals(data.animals, type);
@@ -35,50 +87,59 @@ function loadMissingFoundAnimalsToPage(){
         .catch(error => console.error('Fehler beim Laden der Tiere:', error));
 }
 
-function displayAnimals(animals, type){
-     if (!Array.isArray(animals)) {
-         console.error('Die übergebenen Tiere sind kein Array:', animals);
-         let container, template, heading, subheading, firstAnimalContainer;
-         return;
-        }
-     console.log(animals);
-     
-     if (type === "Vermisste Tiere"){
+function displayAnimals(animals, type) {
+    if (!Array.isArray(animals)) {
+        console.error('Die übergebenen Tiere sind kein Array:', animals);
+        let container, template, heading, subheading, firstAnimalContainer;
+        return;
+    }
+    console.log(animals);
+
+    if (type === "Vermisste Tiere") {
         container = document.querySelector('#missingAnimals');
         template = document.querySelector('#hiddenTemplateMissingAnimals');
-        firstAnimalContainer = document.querySelector('#hiddenFirstMissingAnimal'); 
+        firstAnimalTemplate = document.querySelector('#hiddenFirstMissingAnimalTemplate');
         heading = "Vermisst";
         subheading = "Verschwunden";
     } else if (type === "Gefundene Tiere") {
         container = document.querySelector('#foundAnimals');
         template = document.querySelector('#hiddenTemplateFoundAnimals');
-        firstAnimalContainer = document.querySelector('#hiddenFirstFoundAnimal');
+        firstAnimalTemplate = document.querySelector('#hiddenFirstFoundAnimalTemplate');
         heading = "Gefunden";
         subheading = "Aufgetaucht";
 
     }
 
-   if (animals.length > 0) {
-      container.classList.remove('hidden');
+    if (animals.length > 0) {
+        container.classList.remove('hidden');
 
-       container.querySelector('.heading').innerHTML = type;
+        container.querySelector('.heading').innerHTML = type;
+        container.querySelector('.underHeadline').classList.remove('hidden')
 
-       //------------Breites Feld für missingFoundAnimal------------------------------
-        firstAnimalContainer.getElementsByTagName('h3')[0].innerHTML = heading;
-        firstAnimalContainer.querySelector('.firstAnimalId').innerHTML = animals[0].VermisstGefundenTierID;
-        firstAnimalContainer.querySelector('.firstAnimalImage').src= animals[0].Bildadresse;
-        firstAnimalContainer.querySelector('.firstAnimalDate').innerHTML='<span class="boldText animalDate">am: </span> ' + formatDate(animals[0].Datum);
-        firstAnimalContainer.querySelector('.firstAnimalPlace').innerHTML='<span class="boldText">in: </span>' + animals[0].Ort;
-        firstAnimalContainer.querySelector('.firstAnimalDescription').innerHTML=animals[0].Beschreibung;
+        //------------Breites Feld für missingFoundAnimal------------------------------
+        let cloneFirstAnimal = firstAnimalTemplate.cloneNode(true);
+        const copyFirstAnimalHere = document.getElementById(`copyFirst${type==="Vermisste Tiere" ? 'Missing' : 'Found'}AnimalHere`);
+        console.log(`copyFirst${type==="Vermisste Tiere" ? 'Missing' : 'Found'}AnimalHere`)
+        copyFirstAnimalHere.appendChild(cloneFirstAnimal);
 
-        firstAnimalContainer.classList.remove('hidden');
+        cloneFirstAnimal.getElementsByTagName('h3')[0].innerHTML = heading;
+        cloneFirstAnimal.querySelector('.firstAnimalId').innerHTML = animals[0].VermisstGefundenTierID;
+        cloneFirstAnimal.querySelector('.firstAnimalImage').src = animals[0].Bildadresse;
+        cloneFirstAnimal.querySelector('.firstAnimalDate').innerHTML = '<span class="boldText animalDate">am: </span> ' + formatDate(animals[0].Datum);
+        cloneFirstAnimal.querySelector('.firstAnimalPlace').innerHTML = '<span class="boldText">in: </span>' + animals[0].Ort;
+        cloneFirstAnimal.querySelector('.firstAnimalDescription').innerHTML = animals[0].Beschreibung;
+
+        cloneFirstAnimal.classList.remove('hidden');
+
+        cloneFirstAnimal.id="";
+        cloneFirstAnimal.setAttribute('data-firstAnimal-id', animals[0].VermisstGefundenTierID);
 
         //----------Dynamisches Anlegen der maximal vier Tiere ------------------------
         let counter = 0;
         const copyHere = document.getElementById(`copyAll${type === "Vermisste Tiere" ? 'Missing' : 'Found'}AnimalsHere`)
         let outsideDiv;
 
-        for(let i= 1; i < animals.length; i++) {
+        for (let i = 1; i < animals.length; i++) {
             if (counter % 4 === 0) {
                 outsideDiv = document.createElement('div');
                 outsideDiv.classList.add('box-containerUnsereTiere');
@@ -111,19 +172,8 @@ function displayAnimals(animals, type){
 
             counter++;
         }
-   } else {
-            displayErrorMessage(type, "Keine Tiere gefunden.");
-   }
-}
-
-function headers(type){
-
-    if (type ==="Vermisste Tiere"){
-        return ["Vermisst", "Verschwunden"];
-    } else if (type === "Gefundene Tiere"){
-        return ["Gefunden", "Aufgetaucht"];
     } else {
-        return ["Unbekannter Typ", "Unbekannter Typ"]
+        displayErrorMessage(type, "Keine Tiere gefunden.");
     }
 }
 
@@ -147,4 +197,8 @@ function checkLoginStatus(){
 function formatDate(dateString){
     const [year, month, day] = dateString.split(" ")[0].split("-")
     return `${day}.${month}.${year}`;
+}
+
+function searchType(){
+    const typeInput = document.querySelector('select[name=tierstatusAuswählen]')
 }
